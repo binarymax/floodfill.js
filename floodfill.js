b4593db1f9a0b49b3c08131a8a0eb123a81554bd
Bryan1001 <bryan1001@users.noreply.github.com>
2016-03-27 07:42:05 -0400

    In the case of mw==w or me==e the bound wasn't being honored due to the add assignment operator being evaluated as part of the conditional.

--- floodfill.js
+++ floodfill.js
@@ -20,43 +20,43 @@ function floodfill(x,y,fillcolor,ctx,width,height,tolerance) {
 			e = i;
 			w = i;
 			mw = parseInt(i/w2)*w2; //left bound
-			me = mw+w2;	//right bound			
-			while(mw<(w-=4) && pixelCompareAndSet(w,targetcolor,targettotal,fillcolor,data,length,tolerance)); //go left until edge hit
-			while(me>(e+=4) && pixelCompareAndSet(e,targetcolor,targettotal,fillcolor,data,length,tolerance)); //go right until edge hit
+			me = mw+w2;	//right bound
+			while(mw<w && mw<(w-=4) && pixelCompareAndSet(w,targetcolor,targettotal,fillcolor,data,length,tolerance)); //go left until edge hit
+			while(me>e && me>(e+=4) && pixelCompareAndSet(e,targetcolor,targettotal,fillcolor,data,length,tolerance)); //go right until edge hit
 			for(var j=w;j<e;j+=4) {
 				if(j-w2>=0 		&& pixelCompare(j-w2,targetcolor,targettotal,fillcolor,data,length,tolerance)) Q.push(j-w2); //queue y-1
 				if(j+w2<length	&& pixelCompare(j+w2,targetcolor,targettotal,fillcolor,data,length,tolerance)) Q.push(j+w2); //queue y+1
-			} 			
+			}
 		}
 	}
 	ctx.putImageData(img,0,0);
 }
 
-function pixelCompare(i,targetcolor,targettotal,fillcolor,data,length,tolerance) {	
+function pixelCompare(i,targetcolor,targettotal,fillcolor,data,length,tolerance) {
 	if (i<0||i>=length) return false; //out of bounds
 	if (data[i+3]===0)  return true;  //surface is invisible
-	
+
 	if (
-		(targetcolor[3] === fillcolor.a) && 
-		(targetcolor[0] === fillcolor.r) && 
-		(targetcolor[1] === fillcolor.g) && 
+		(targetcolor[3] === fillcolor.a) &&
+		(targetcolor[0] === fillcolor.r) &&
+		(targetcolor[1] === fillcolor.g) &&
 		(targetcolor[2] === fillcolor.b)
 	) return false; //target is same as fill
-	
+
 	if (
 		(targetcolor[3] === data[i+3]) &&
-		(targetcolor[0] === data[i]  ) && 
+		(targetcolor[0] === data[i]  ) &&
 		(targetcolor[1] === data[i+1]) &&
 		(targetcolor[2] === data[i+2])
-	) return true; //target matches surface 
-	
+	) return true; //target matches surface
+
 	if (
 		Math.abs(targetcolor[3] - data[i+3])<=(255-tolerance) &&
-		Math.abs(targetcolor[0] - data[i]  )<=tolerance && 
+		Math.abs(targetcolor[0] - data[i]  )<=tolerance &&
 		Math.abs(targetcolor[1] - data[i+1])<=tolerance &&
 		Math.abs(targetcolor[2] - data[i+2])<=tolerance
-	) return true; //target to surface within tolerance 
-	
+	) return true; //target to surface within tolerance
+
 	return false; //no match
 }
 
